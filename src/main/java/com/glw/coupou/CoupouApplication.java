66cde41b2b93f16b0b500c3add039ded096e06fa
DragonV96 <510537547@qq.com>
2019-12-04 00:47:13 +0800

    【init】初始化项目配置

---
+++ src/main/java/com/glw/coupou/CoupouApplication.java
@@ -0,0 +1,13 @@
+package com.glw.coupou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoupouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoupouApplication.class, args);
+    }
+
+}
